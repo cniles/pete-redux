e64e73dba8521a113c29f2a117dea8c88524a175
niles.c <niles.c@gmail.com>
2011-03-15 15:00:54 +0000

    begin integration of bullet physics objects int GameObject class

--- gameobject.h
+++ gameobject.h
@@ -3,10 +3,24 @@
 
 #include "bulletwrap.h"
 
+class GameObject;
+class GameObjectMotionState : public btMotionState {
+  GameObjectMotionState(const btTransform& initial_position, GameObject* object);
+  inline void setObject(GameObject* object) { this->object = object; }
+  ~GameObjectMotionState();
+  virtual void getWorldTransform(btTransform& world_transform);
+  virtual void setWorldTransform(const btTransform& world_transform);
+ protected:
+  GameObject* object;
+  btTransform position;
+};
+
 class GameObject {
  protected:
   btVector3 position;
   int direction;
+  btCollisionObject* collision_object;
+  btRigidBody* rigid_body;
  public:
   GameObject(float x, float y);
   inline btVector3 getPosition() {return position;}
