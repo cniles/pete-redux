3913808e95e43f11668f6b5d2ad0b81b68bdeeff
niles.c@gmail.com <niles.c@gmail.com@87077d0b-f1d3-e147-9c16-c81f023e90d5>
2011-03-15 15:00:54 +0000

    begin integration of bullet physics objects int GameObject class

--- trunk/gameobject.cpp
+++ trunk/gameobject.cpp
@@ -1,5 +1,23 @@
 #include "gameobject.h"
 
+GameObjectMotionState::GameObjectMotionState(const btTransform& initial_position, GameObject* object) 
+  : position(initial_position), object(object) {
+  
+}
+
+GameObjectMotionState::~GameObjectMotionState() {
+
+}
+
+void GameObjectMotionState::getWorldTransform(btTransform& world_transform) {
+  world_transform = position;
+}
+
+void GameObjectMotionState::setWorldTransform(const btTransform& world_transform) {
+  
+}
+
+
 GameObject::GameObject(float x, float y) {
   position = btVector3(x, y, 0);
   direction = -1;
