561ee0b7c65dc1888278c0a8bac573a1f412d8e4
niles.c <niles.c@gmail.com>
2011-03-20 01:06:56 +0000

    *Bat source

---
+++ bat.h
@@ -0,0 +1,36 @@
+#ifndef BAT_H_
+#define BAT_H_
+
+#include <BulletCollision/CollisionShapes/btBox2dShape.h>
+#include "statemachineobject.h"
+
+class Bat : public StateMachineObject {
+  const static int INITIAL_HEALTH = 1;
+  const static float MASS = 1.0f;
+  const static float ATTACK_COOLDOWN = 0.5f;
+  const static float GRAVITY = 0.0f;
+  
+  static CollisionScheme collision_scheme;
+  static btBox2dShape collision_shape;
+  static Animation animation;
+
+  enum SubState {
+    MOVING, PAUSING
+  };
+
+  int state_flag;
+  int timer;  
+  int health;
+  bool can_attack;
+  float attack_timer;
+
+  MAKE_STATE(StateMove, Bat);
+  MAKE_STATE(StateAttack, Bat);
+  MAKE_STATE(StateDead, Bat);
+
+ public:
+  Bat(GameState*, btVector3 position);
+  static void loadStaticAssets();
+};
+
+#endif
