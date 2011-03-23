e38ac4c777476a7c448106454df65b8e24db2c5f
niles.c@gmail.com <niles.c@gmail.com@87077d0b-f1d3-e147-9c16-c81f023e90d5>
2011-03-23 17:38:23 +0000

    files for previous commit

---
+++ trunk/src/darkchampion.cpp
@@ -0,0 +1,25 @@
+ #include "darkchampion.h"
+Animation DarkChampion::animation;
+
+void DarkChampion::StateIdle::onEnter() {}
+void DarkChampion::StateIdle::onUpdate(float dt) {}
+void DarkChampion::StateIdle::onLeave() {}
+
+void DarkChampion::StateMove::onEnter() {}
+void DarkChampion::StateMove::onUpdate(float dt) {}
+void DarkChampion::StateMove::onLeave() {}
+
+void DarkChampion::StateDead::onEnter() {}
+void DarkChampion::StateDead::onUpdate(float dt) {}
+void DarkChampion::StateDead::onLeave() {}
+
+DarkChampion::DarkChampion(GameState* gamestate, btVector3 position) 
+  : StateMachineObject(gamestate, position, &animation, PhysicsObject::default_collision_scheme, new DarkChampion::StateIdle(this)) {
+  animation_timer.setClip(2);
+}
+
+void DarkChampion::loadStaticAssets() {
+  std::cerr << "Loading dark...";
+  animation = Animation("gfx/darkchampion");
+  std::cerr << "loaded!" << std::endl;
+}
